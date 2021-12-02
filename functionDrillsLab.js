65d195e45168372008d9cf8c9a9ce760591851e7
McKenzie Allen <mckenzie.allen144@gmail.com>
2021-12-02 01:06:13 -0700

    all questions answered

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -303,7 +303,9 @@ let sampleString = "Hi, my name is Kylo."
     */
 
 //CODE HERE
+const upperCase = (str) => { console.log(str.toUpperCase()) }
 
+upperCase(sampleString)
 
 ////////////////// PROBLEM 18 ////////////////////
 /*
@@ -316,31 +318,85 @@ let sampleString = "Hi, my name is Kylo."
   return 'must provide a valid email address'
 */
 
-////////////////// PROBLEM 19 ////////////////////
-/*
-  Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were able to purchase.
-  Create a variable called `totalFrogs` and set it equal to your function invoked, passing in the amount of gold you are willing to spend.
-*/
-
 //CODE HERE
-
-
-////////////////// PROBLEM 20 ////////////////////
-/*
-  You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... However, you can't really go to a store and by 1.333 products. You would just be able to purchase 1 product. Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. Invoke the function and store the returned value to a variable called `totalFrogs2`.
-*/
-
-//CODE HERE
-
-
-////////////////// PROBLEM 21 ////////////////////
-let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9]
+function emailCheck(email) {
+    let stringEmail = String(email)
+    let trimEmail = stringEmail.trim()
+    if (trimEmail.includes("@")) {
+        return "email verified"
+    } else {
+        return "must provide a valid email address"
+    }
+}
+console.log(emailCheck(" mckenzie.allen144@gmail.com"))
+    ////////////////// PROBLEM 19 ////////////////////
     /*
-      Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
+      Write a function, naming it whatever you believe to be appropriate, that buys as many chocolate frogs as possible with a certain amount of gold. Each chocolate frog costs 3 gold. Your function should take in a single parameter, which is the amount of gold you are willing to spend. Your function should return a total amount of chocolate frogs you were able to purchase.
+      Create a variable called `totalFrogs` and set it equal to your function invoked, passing in the amount of gold you are willing to spend.
     */
 
 //CODE HERE
+function buyTheFrogs(gold) {
+    if (gold % 3 === 0) {
+        return gold / 3
+    } else if ((gold - 1) % 3 === 0) {
+        return (gold - 1) / 3
+    } else if ((gold - 2) % 3 === 0) {
+        return (gold - 2) / 3
+    }
+}
+const totalFrogs = buyTheFrogs(24)
 
+console.log(totalFrogs)
+    ////////////////// PROBLEM 20 ////////////////////
+    /*
+      You might have noticed a slight bug in the previous problem. If you were to pass in 4 gold, the function would return to you 1.3333... 
+      However, you can't really go to a store and by 1.333 products.
+       You would just be able to purchase 1 product. 
+       Re-write the function you used in the previous problem (give it the same name, just add a 2 to the end of it) that fixes this bug. 
+       Invoke the function and store the returned value to a variable called `totalFrogs2`.
+    */
+
+//CODE HERE
+function buyTheFrogs2(gold) {
+    if (gold % 3 === 0) {
+        return gold / 3
+    } else if ((gold - 1) % 3 === 0) {
+        return (gold - 1) / 3
+    } else if ((gold - 2) % 3 === 0) {
+        return (gold - 2) / 3
+    }
+}
+const totalFrogs2 = buyTheFrogs2(68)
+
+console.log(totalFrogs2)
+
+////////////////// PROBLEM 21 ////////////////////
+let sampleArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    /*
+      Write a function that takes in an array of numbers as an argument. 
+      In the body of the function, write logic to determine if the array is in ascending order. 
+      The function should return true, if it is sorted in ascending order, false if it is not. 
+      Create a variable, `arrayIsAscending` and set it equal to your function invoked. 
+      Use the sample array to test this function.
+    */
+
+//CODE HERE
+function assendingArray(arr) {
+    let value = arr[0]
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] <= value) {
+            return false
+        } else {
+            value = arr[i]
+        }
+    }
+    return true
+}
+
+const arrayIsAscending = assendingArray(sampleArray)
+
+console.log(arrayIsAscending)
 
 ////////////////// PROBLEM 22 ////////////////////
 
@@ -365,13 +421,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ["duck"]
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ["duck, rubberDuck, sailorDuck"]
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ["duck, sailorDuck"]
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ["duck, realDuck"]
